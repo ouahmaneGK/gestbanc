ffb02c480ab601cdbe1f940eaec79208a4079ce3
ouahmaneGK <o.ouahmane@gmail.com>
2019-07-15 13:27:47 +0200

    add annotations to admin

--- src/main/java/com/wha/spring/model/Administrator.java
+++ src/main/java/com/wha/spring/model/Administrator.java
@@ -1,5 +1,17 @@
 package com.wha.spring.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "Administrator")
 public class Administrator {
 
 }
