c2c496a92c4ed3a70169dcd571940fea7962ba25
amaria-foukia <amaria.foukia@gmail.com>
2019-07-15 16:34:57 +0200

    creation branche gestionTransaction, classes Transaction, Credit, Debit

---
+++ src/main/java/com/wha/spring/model/Debit.java
@@ -0,0 +1,24 @@
+package com.wha.spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Debit extends Transaction{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+}
---
+++ src/main/java/com/wha/spring/model/Transaction.java
@@ -0,0 +1,29 @@
+package com.wha.spring.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int montant;
+	//private Compte compte;
+	private String libelle; 
+	private Date date;
+
+}
