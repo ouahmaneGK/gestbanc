3a73ba24ff632f397eb4606cc3829381f70cab2f
Mistigri <marie-lacour@outlook.fr>
2019-07-15 16:51:44 +0200

    ajout classe Notification

---
+++ src/main/java/com/wha/spring/model/Notification.java
@@ -0,0 +1,37 @@
+package com.wha.spring.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Notification")
+public class Notification {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "DATE", nullable = false)
+	private Date date;
+	
+	@Column(name = "MESSAGE", nullable = false)
+	private String message ;
+	
+}
